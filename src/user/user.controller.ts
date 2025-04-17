aa7f0821b187bc6bd55e37818ff886daec4fa12f
Md. Ahsanul Hasan <82327664+AhsanulHasan16@users.noreply.github.com>
2025-04-17 21:51:08 +0600

    Added custom RolesGuard and Roles decorator
    Created a custom RolesGuard to handle authorization based on user roles.
    Added a Roles decorator to set role metadata on protected routes.
    Applied the guard to specifically the route to create user to restrict access to only admins.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,17 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/CreateUser.dto";
+import { AuthGuard } from "@nestjs/passport";
+import { RolesGuard } from "src/roles.guard";
+import { Roles } from "src/roles.decorator";
 
 
 @Controller('user')
 export class UserController { 
     constructor(private userService: UserService) { }
     
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @Roles('admin')
     @Post('create')
     async register(@Body() createUserDto: CreateUserDto) {
         return this.userService.createUser(createUserDto);
